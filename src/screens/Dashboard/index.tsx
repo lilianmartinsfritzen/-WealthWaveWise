af65f9669304ca4fb2c2a0e1230b4d2cd00a83e8
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2024-02-26 22:54:28 -0300

    feature(dev): dashboard started

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export function Dashboard() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Dashboard</Text>
+    </View>
+  );
+}
